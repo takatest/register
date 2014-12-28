6890efa5c20d74a9088556739074b3838a7ac07d
takatest <taka.komuro1@gmail.com>
2014-12-28 15:48:08 +1100

    init first

---
+++ RetailWebTest.java
@@ -0,0 +1,155 @@
+package RegisterdomainName;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Ignore;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+
+import static org.junit.Assert.*;
+
+public class RetailWebTest {
+
+	private int 						k;					// counter
+	private int 						year = 1;			// year 1,2
+	private FirefoxDriver 				driver;				// firefox driver
+	private String 						url;				// URL
+	private String 						myaccount;			// my account			property
+	private String 						password;			// my account password	property
+    private static int					domain_num = 3;		// number of domain
+	private static int 					space = 3;			// domain space
+	private static String[] 			domain_space = {".com",".net",".org",".biz",".info",".melbourne"};// domain space
+    private static SearchDomainPage 	searchPage;			// search domain page
+    
+    private static LoginPage 	 		loginPage;			// login page
+    private static ReviewPage			ReviewPage;			// Review page
+    private static PaymentPage			PaymentPage;		// Payment page
+    private static CompletePage			CompletePage;		// Complete page
+    private static LogoutPage			logOutPage;			// Log out page
+    
+	private String baseUrl = "https://stage.melbourneit.com.au/";		//URL stage
+//	private Sstring baseUrl = "https://www.melbourneit.com.au/";		//URL prod
+    
+    
+	@BeforeTest
+	public void BeforeTest() {
+		 driver = new FirefoxDriver();
+//		 driver.get(baseUrl); 
+		 
+	    // create page object
+        searchPage = new SearchDomainPage();
+        loginPage = new LoginPage();
+        ReviewPage = new ReviewPage();
+        PaymentPage = new PaymentPage();
+        CompletePage = new CompletePage();
+        logOutPage = new LogoutPage();
+        
+        
+        System.out.println("BeforeTest");
+        
+        Properties prop = new Properties();
+    	InputStream input = null;
+     
+    	try {
+    		input = new FileInputStream("config.properties");
+     
+    		// load a properties file
+    		prop.load(input);
+     
+    		// get the property value and print it out
+    		myaccount = prop.getProperty("myaccount");
+      		password = prop.getProperty("password");
+      		System.out.println(myaccount);
+    		System.out.println(password);
+    	     
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+    	} finally {
+    		if (input != null) {
+    			try {
+    				input.close();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	}
+	}
+
+	@BeforeMethod
+	public void BeforeMethod(){
+		driver.get(baseUrl); 
+	}
+	
+	// set up data
+	@DataProvider(name = "GetDomain")
+	public static Object[][] domainName() {
+		Object[][] data = new Object[domain_num][2];
+		for (int k = 0; k<domain_num; k++){
+			String domainName = searchPage.CreateDomainName(domain_space[k]);
+			data[k][0]= k+1;
+			data[k][1]= domainName;
+		}
+		return data;
+	}
+	 	
+/********************************************************************************************
+ * 	
+ * Register domain name test
+ * 
+ ********************************************************************************************/
+	@Test(dataProvider = "GetDomain",priority = 0)
+	public void RegisterDomain(int number, String domainName){
+	
+		// crate domain name
+//		String domainName = searchPage.CreateDomainName(domain_space[space]);
+		//Search domain page
+		
+	     System.out.println("-------------------"+number+"-"+domainName+"-------------------------");
+		searchPage.searchDomain(driver, domainName);
+		
+	     System.out.println("search page");
+	     
+		//login page
+		loginPage.loginmyaccount(driver,baseUrl, myaccount, password);
+	     System.out.println("login");
+
+		//Review your order 1 year
+		ReviewPage.OneYearOrder(driver, year);
+	     System.out.println("Review end");
+
+		// Payment Page
+		PaymentPage.PaybyCredit(driver, baseUrl);
+	     System.out.println("Payment end");
+	     
+		// Complete Page
+		CompletePage.CompleteSuccess(driver);
+		System.out.println("Complete end");
+	
+		// Log out Page
+		logOutPage.logout(driver, baseUrl); 
+		System.out.println("log out My account");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		System.out.println("AfterTest");
+		driver.quit();
+	}
+	
+}
---
+++ SearchDomainPage.java
@@ -0,0 +1,60 @@
+package RegisterdomainName;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Ignore;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+
+import static org.junit.Assert.*;
+
+public class SearchDomainPage extends Page {
+
+	public void searchDomain(WebDriver driver, String domain) {
+
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By
+				.cssSelector("div.element-group > input[name=\"query\"]")));
+
+		driver.findElement(
+				By.cssSelector("div.element-group > input[name=\"query\"]"))
+				.clear();
+		driver.findElement(
+				By.cssSelector("div.element-group > input[name=\"query\"]"))
+				.sendKeys(domain);
+
+		driver.findElement(By.cssSelector("button[type=\"submit\"]")).click(); // search
+																				// button
+		By btn = By.xpath("//div[@id='main']/div/div/div/div[2]/div/div/div/button");
+
+		waitForAWhile(5000);
+
+		// assertEquals("is available",
+		// driver.findElement(By.cssSelector("h2.ng-binding.positive")).getText());
+		// // is available
+		WebElement we = driver.findElement(btn);
+		we.click(); // add button
+
+	}
+
+	/* 
+	 * create Domain name
+	 */
+	public String CreateDomainName(String domainSpace) {
+		
+		int num = (int) (Math.random() * 50 + 1);
+		String domainName = "takatakatest" + num;
+		domainName = domainName + String.valueOf(num)+ domainSpace;
+
+		return domainName;
+	}
+
+}
